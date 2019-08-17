b37c39240205b8cf693070bff39fdadd5208ed7e
Ravin Singh D <ravin@getcloudcherry.com>
2019-08-17 23:17:15 +0530

    Created custom elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(injector: Injector) {
+    const PopupElement = createCustomElement(AppComponent, { injector });
+    customElements.define('popup-element', PopupElement);
+  }
+
+  ngDoBootstrap() {}
+}
