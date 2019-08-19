a6f9c7a6b2928c6fd5c8aca5825102c2c947d1fa
Ravin Singh D <ravin@getcloudcherry.com>
2019-08-19 08:42:11 +0530

    Passing input to angular elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
     <input [(ngModel)]="name" />
     Hello {{ name }}
+    <div>Data String: {{ dataString }}</div>
+    <div>{{ data | json }}</div>
   `,
   styles: []
 })
 export class AppComponent {
+  @Input() dataString = '';
+  @Input() data = { hi: 'hello' };
   name = 'World';
 }
